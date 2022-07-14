824249a89e3e325151349d6a9b03d1d5976ddfdf
ccc <156697004@qq.com>
2022-07-15 04:54:01 +0800

    添加 布局路由&视图路由

---
+++ src/views/auth/login.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    auth.login
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/error/404.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    error.404
+  </div>
+</template>
+
+<style>
+
+</style>
